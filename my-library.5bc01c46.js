d480815b714fce3db28b3ebf177ba518f97364b9
VeronikaSavluk <98710326+VeronikaSavluk@users.noreply.github.com>
2022-09-03 22:44:14 +0000

    Deploying to gh-pages from @ VeronikaSavluk/test-js-project@7c0334db0bb827d048289eee3833db3bdc122e76 🚀

---
+++ my-library.5bc01c46.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".header-search__form"),gallery:document.querySelector(".gallery"),buttons:document.querySelector(".pages"),API_key:"4acb7b5ad40671c4d36a93ebc82fda39",BASE_URL:"https:api.themoviedb.org/3/trending",page:1,totalPages:0};!function(){const t=Array(e.totalPages).fill().map(((e,t)=>`<button data-page=${t}>${t+1}</button>`)).join(" ");e.buttons.innerHTML="",e.buttons.insertAdjacentHTML("beforeend",`${t}`)}(),e.buttons.addEventListener("click",(function(t){e.page=t.target.dataset.page}));
+//# sourceMappingURL=my-library.5bc01c46.js.map
