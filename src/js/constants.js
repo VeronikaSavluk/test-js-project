e174492a6dc76c635729c1e7a309f15ccd15b8fe
Veronika Savluk <s_veronica@ukr.net>
2022-09-04 01:41:56 +0300

    Update constants.js

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,6 +1,9 @@
 export const refs = {
+    form: document.querySelector(".header-search__form"),
     gallery: document.querySelector(".gallery"),
     buttons: document.querySelector(".pages"),
+    API_key: `4acb7b5ad40671c4d36a93ebc82fda39`,
+    BASE_URL: `https:api.themoviedb.org/3/trending`,
     page: 1,
-    totalPages: 10,
+    totalPages: 0,
 }
