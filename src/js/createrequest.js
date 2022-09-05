614844a8014c702da663b25e16532448a85f6923
Veronika Savluk <s_veronica@ukr.net>
2022-09-06 02:45:53 +0300

    Create createrequest.js

---
+++ src/js/createrequest.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { refs } from "./constants";
+let page = 1;
+let id = 0;
+
+export default async function createRequest() {
+  try {
+    const primaryURL = `/trending/movie/day?api_key=${refs.API_key}`;
+    let individualURL = `/movie/${id}?api_key=${refs.API_key}`;
+    let URLBySearch = `/search/movie?api_key=${refs.API_key}&language=en-US&query=${refs.query}&page=${page}&include_adult=false`;
+    const URLs = [
+      primaryURL,
+      individualURL,
+      URLBySearch
+    ];
+
+    const arrayofPromises = URLs.map(async (URLs) => {
+      return await axios.get(`${refs.BASE_URL}${URLs}`);
+    });
+
+    const promise = await Promise.allSettled(arrayofPromises);
+    return promise;
+  } catch (error) {
+    console.error(error);
+  }
+}
