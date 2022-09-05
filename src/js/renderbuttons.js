0289d701cb7544af92d7dff82d56ca140e404a6c
Veronika Savluk <s_veronica@ukr.net>
2022-09-06 02:46:32 +0300

    Update renderbuttons.js

--- src/js/renderbuttons.js
+++ src/js/renderbuttons.js
@@ -1,7 +1,8 @@
 import { refs } from './constants';
+
 export default function renderButtons() {
     const buttons = Array(refs.totalPages).fill().map((_, idx) =>
-        `<button data-page=${idx}>${idx + 1}</button>`).join(" ");
+        `<li><button data-page=${idx}>${idx + 1}</button></li>`).join(" ");
     refs.buttons.innerHTML = "";
-    refs.buttons.insertAdjacentHTML('beforeend', `${buttons}`);
+    refs.buttons.insertAdjacentHTML('beforeend', `<li><button><<</button></li>${buttons}<li><button>>></button></li>`);
 };
