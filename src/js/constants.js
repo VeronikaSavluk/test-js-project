b617771520ac73d1bc9280c3a2f9856a0195b224
Veronika Savluk <s_veronica@ukr.net>
2022-09-07 03:16:21 +0300

    Update constants.js

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,6 +1,7 @@
 export const refs = {
     form: document.querySelector("#search-form"),
     gallery: document.querySelector(".gallery-items"),
+    movieCards: document.querySelectorAll(".gallery-card"),
     buttons: document.querySelector(".pages"),
     API_key: `4acb7b5ad40671c4d36a93ebc82fda39`,
     BASE_URL: `https:api.themoviedb.org/3`,
@@ -9,4 +10,5 @@ export const refs = {
     totalPages: 0,
     movies: null,
     movieGenre: null,
+    movieId: null,
 }
