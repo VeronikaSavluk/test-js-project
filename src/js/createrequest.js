26438f7c230638255564b51d252267160973396c
Veronika Savluk <s_veronica@ukr.net>
2022-09-07 03:16:06 +0300

    Update createrequest.js

--- src/js/createrequest.js
+++ src/js/createrequest.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 import { refs } from "./constants";
 let page = 1;
-let id = 0;
 
 export default async function createRequest() {
   try {
     const primaryURL = `/trending/movie/day?api_key=${refs.API_key}`;
-    let individualURL = `/movie/${id}?api_key=${refs.API_key}`;
+    let individualURL = `/movie/${refs.movieId}?api_key=${refs.API_key}`;
     let URLBySearch = `/search/movie?api_key=${refs.API_key}&language=en-US&query=${refs.query}&page=${page}&include_adult=false`;
     const URLs = [
       primaryURL,
