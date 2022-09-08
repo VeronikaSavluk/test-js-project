7b7acc6fc0f5a656d9020b58daf14290020b2e54
Veronika Savluk <s_veronica@ukr.net>
2022-09-09 02:56:08 +0300

    Update renderbuttons.js

--- src/js/renderbuttons.js
+++ src/js/renderbuttons.js
@@ -1,8 +1,19 @@
+import Pagination from 'tui-pagination';
+
+import 'tui-pagination/dist/tui-pagination.min.css';
+
 import { refs } from './constants';
 
 export default function renderButtons() {
-    const buttons = Array(refs.totalPages).fill().map((_, idx) =>
-        `<li><button data-page=${idx}>${idx + 1}</button></li>`).join(" ");
-    refs.buttons.innerHTML = "";
-    refs.buttons.insertAdjacentHTML('beforeend', `<li><button><<</button></li>${buttons}<li><button>>></button></li>`);
+    // const buttons = Array(refs.totalPages).fill().map((_, idx) =>
+    //     `<button class="page__button" data-page=${idx}>${idx + 1}</button>`).join(" ");
+    // refs.buttons.innerHTML = "";
+    // refs.buttons.insertAdjacentHTML('beforeend', `${buttons}`);
+    const options = {
+    totalItems: refs.totalPages,
+    itemsPerPage: 10,
+    visiblePages: 5,
+    centerAlign: true
+}
+    const pagination = new Pagination(refs.buttons, options);
 };
